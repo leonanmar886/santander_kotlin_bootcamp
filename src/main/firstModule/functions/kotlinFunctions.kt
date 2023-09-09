0a8f04e36f5ba6faaed20c9b59256a52d384d910
Leonan Marques <leonan.marques886@gmail.com>
2023-09-09 13:18:45 -0300

    multiples functions have been implemented

---
+++ src/main/firstModule/functions/kotlinFunctions.kt
@@ -0,0 +1,28 @@
+package src.main.firstModule.functions
+
+fun main(){
+    println("Hello World!")
+    printMessageWithPrefix("Hello", "Log") // function call
+    printMessageWithPrefix("Hello") // function call
+    println(sum(1, 2)) // function call
+    println(multiply(2, 4)) // function call
+    printAll("Hello", "World", "Kotlin") // function call
+    printAll(*arrayOf("Hello", "World", "Kotlin")) // spread operator
+    val x = 2
+    val y = 3
+    println("$x + $y = ${x + y}") // string interpolation
+}
+
+fun printMessageWithPrefix(message: String, prefix: String = "Info:"){// the prefix argument has a default value
+    println("[$prefix] $message") // string interpolation
+}
+
+fun sum(x: Int, y: Int): Int{ // this function mandatory returns an Int
+    return x + y
+}
+
+fun multiply(x: Int, y: Int) = x * y // kotlin functions syntax don't need the brackets if the function is a single expression
+
+fun printAll(vararg messages: String){ // vararg is used to pass a variable number of arguments
+    for (m in messages) println(m) // for loop that executes a inlined function
+}
